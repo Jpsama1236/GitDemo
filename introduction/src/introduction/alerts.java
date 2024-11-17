f2c84aac76f64aa60723f430a984d0b42afb3c90
JP <samajayaprakash123@gmail.com>
2024-11-17 16:46:16 +0530

    deleted stub

--- introduction/src/introduction/alerts.java
+++ introduction/src/introduction/alerts.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class alerts {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		driver.findElement(By.id("name")).sendKeys("Rahul");
